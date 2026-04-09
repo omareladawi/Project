a38600a93276e07d92c1145eb0499723a9ead80c
R3p0 <139224742+omareladawi@users.noreply.github.com>
2026-04-09 20:15:55 +0200

    Delete web-scanner-main directory

--- web-scanner-main/src/web_scanner/scanner/modules/__init__.py
+++
@@ -1,5 +0,0 @@
-from .brute_force import BruteForceModule
-from .exploit_executor import ExploitModule
-from .reconnaissance import ReconnaissanceModule
-
-__all__ = ['BruteForceModule', 'ExploitModule', 'ReconnaissanceModule']
